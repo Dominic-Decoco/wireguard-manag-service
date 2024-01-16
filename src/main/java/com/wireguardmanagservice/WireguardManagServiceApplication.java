86ec34ba0dfc7443ebc6bda91a719e5c79c57a50
Dominic-Decoco <domindecoco@gmail.com>
2024-01-16 18:35:38 +0300

    created a spring + vue project

---
+++ src/main/java/com/wireguardmanagservice/WireguardManagServiceApplication.java
@@ -0,0 +1,13 @@
+package com.wireguardmanagservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WireguardManagServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WireguardManagServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wireguardmanagservice/WireguardManagServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wireguardmanagservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WireguardManagServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
